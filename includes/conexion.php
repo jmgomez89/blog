f3c4befceda538cbb8440a8388efc7d5b865097c
jmgomez89 <jmgomezn8@gmail.com>
2022-07-28 19:40:38 -0300

    final

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,10 +1,10 @@
 <?php
 //Conexión a la base de datos
 
-$server = '45.132.157.182';
-$username = 'u921038692_juanmgomez';
-$password = 'Cachas2020';
-$database = 'u921038692_blog';
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'blog';
 
 $conexion_db = mysqli_connect($server, $username, $password, $database) or exit ("No se pudo conectar a la base de datos");
 
